d4759e922ddae82b2469f9c65124a1ad4c600d3b
DEAD1nsane <turbolaceup@gmail.com>
2026-08-04 03:27:47 -0500

    update main.py

--- main.py
+++ main.py
@@ -322,3 +322,7 @@ def send_audio_track(chat_id, msg_id, key, file_path, title, performer, is_priva
                 CACHED_FILE_IDS[key] = msg.audio.file_id
     except Exception as send_err:
         print(f"Error sending audio ({key}): {send_err}")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
